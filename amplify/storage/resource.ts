5a2daeca244bb6e5c042dbcc404c2244274949ba
Pavithra-fireflies14 <pavithrameganathan0001@gmail.com>
2025-05-15 06:51:09 +0530

    File upload

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,7 +3,7 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'fileuploads',
   access: (allow) => ({
-    '*': [
+    'upload/*': [
       allow.guest.to(['read', 'write', 'delete']),
       allow.authenticated.to(['read', 'write', 'delete']),
     ],
